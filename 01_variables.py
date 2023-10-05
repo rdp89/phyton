0222cc4985539989f3f7f9a2966f4a717d7e79bb
rdp89 <rafadelgado89@gmail.com>
2023-10-05 20:29:30 +0200

    Termino parte de variables

--- 01_variables.py
+++ 01_variables.py
@@ -23,3 +23,21 @@ print(len(my_string_variable))
 # Variables en una sola línea. ¡Cuidado con abusar de esta sintaxis!
 name, surname, alias, age = "Rafa", "Delgado", "RDP89", 34
 print("Me llamo:",name,surname,"y tengo:",age,"años. Y mi alias es:",alias)
+
+# Inputs
+
+first_name = input("What's your name?: ")
+age = input("How old are you?: ")
+
+print(first_name)
+print(age)
+
+# Cambiamos su tipo
+
+first_name = 34
+age = "Rafa"
+
+#Forzamos tipado ¿?
+address: str = "Mi dirección" # Solo indicamos que nos gustaría que fuese tipo string ya que python es un lenguaje de tipado flojo
+address = 32
+print(address)
