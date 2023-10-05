fc9b8f736ae0f79600f7aa034e1f59a0c7131f8b
rdp89 <rafadelgado89@gmail.com>
2023-10-05 12:16:48 +0200

    Se añade el primer fichero de trabajo

---
+++ hello.py
@@ -0,0 +1,20 @@
+# Esto es un comentario: Hola mundo
+# Nuestro hola mundo en python
+print("Hola python")
+print('Hola python')
+
+"""
+Este es un
+comentario
+en varias líneas
+"""
+
+'''
+Este es un
+comentario
+en varias líneas
+'''
+
+# Consultar el tipo de dato
+print(type("Hola python")) # Tipo 'str'
+print(type(5)) # Tipo int
