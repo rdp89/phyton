6e2795ffc0ab7d6bc35dcd4c71ff1137828f9d05
rdp89 <rafadelgado89@gmail.com>
2023-10-10 10:29:48 +0200

    Añadimos listas

---
+++ 04_listas.py
@@ -0,0 +1,15 @@
+### Listas ###
+
+my_list = list() # Forma 1
+my_other_list = [] # Forma 2 (al final es lo mismo)
+
+print(len(my_list)) # 0
+
+my_list = [35, 24, 62, 30, 34]
+
+print(my_list)
+
+my_other_list = [34, 1.76, "Rafa", "Delgado"]
+
+print(type(my_other_list))
+print(type(my_list))
