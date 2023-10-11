4cdb7a9fc9a6a4f0c81affb64eb9194011869921
rdp89 <rafadelgado89@gmail.com>
2023-10-11 13:17:02 +0200

    Pequeña modificación

--- 04_listas.py
+++ 04_listas.py
@@ -1,4 +1,4 @@
-### Listas ###
+### Listas (no es lo mismo que un array)###
 
 my_list = list() # Forma 1
 my_other_list = [] # Forma 2 (al final es lo mismo)
@@ -13,3 +13,11 @@ my_other_list = [34, 1.76, "Rafa", "Delgado"]
 
 print(type(my_other_list))
 print(type(my_list))
+
+# Acceso a la lista
+print(my_other_list[0]) # Primer elemento de la lista
+print(my_other_list[-1]) # Último elemento de la lista, pero esto petaría en otros lenguajes
+#print(my_other_list[8]) Esto da error porque no existe el elemento de la posición 8
+print(my_list.count(62)) # Cuenta las coincidencias dentro de la lista, en este caso, 62 solo hay uno
+age, height, name, surname = my_other_list
+print(name) #Se asigna cada variable al valor que tiene la lista, obviamente, tienen que coincidir, sino peta
