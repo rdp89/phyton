c57d9397fff266408d6e765952c767e87798d89a
rdp89 <rafadelgado89@gmail.com>
2023-10-17 09:41:17 +0200

    Terminamos las listas

--- 04_listas.py
+++ 04_listas.py
@@ -21,3 +21,44 @@ print(my_other_list[-1]) # Último elemento de la lista, pero esto petaría en o
 print(my_list.count(62)) # Cuenta las coincidencias dentro de la lista, en este caso, 62 solo hay uno
 age, height, name, surname = my_other_list
 print(name) #Se asigna cada variable al valor que tiene la lista, obviamente, tienen que coincidir, sino peta
+
+#Asignación manual
+name, height, age, surname = my_other_list[2], my_other_list[1], my_other_list[0], my_other_list[3]
+print(name)
+
+#Se pueden concatenar listas
+print(my_list + my_other_list)
+
+#Añadir elementos a una lista con append()
+
+my_other_list.append("Devops")
+print(my_other_list)
+
+#Insertar con insert() en la posición elegida, se desplazan los elementos de la lista
+my_other_list.insert(1, "Azul")
+print(my_other_list)
+
+my_other_list.remove("Azul") #Eliminamos por coincidencia del valor (Si tenemos dos borra el primero)
+print(my_other_list)
+
+
+print(my_other_list) 
+print(my_other_list.pop()) #Elimina el último elemento de la lista y nos devuelve el valor que ha borrado
+
+print(my_other_list)
+my_pop_element = my_other_list.pop(2) #Guardamos el valor que vamos a borrar
+print(my_other_list.pop(2)) #Le pasamos el índice del elemento de la lista que queremos borrar
+
+del my_list[2] #Con esto borramos sin más el índice 2 de la lista (pide el índice)
+
+my_new_list = my_list.copy() #Copiamos la lista anterior en una nueva lista con copy()
+
+print(my_new_list)
+my_new_list.reverse() #Le da la vuelta a los valores de la lista
+print(my_new_list)
+
+my_new_list.sort() #Ordena los elementos de la lista, como son int, los ordena de menor a mayor
+
+print(my_new_list[0:2]) #Sublistas, va a mostrar lo que hay entre esos dos índices, sin incluir éstos
+
+my_list.clear() #Con esto borramos todos los elementos de la lista (la limpiamos)
