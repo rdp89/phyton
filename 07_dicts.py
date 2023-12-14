4de82804a7187321ae0832a5b26d4ba181108898
rdp89 <rafadelgado89@gmail.com>
2023-12-14 12:04:47 +0100

    diccionarios

---
+++ 07_dicts.py
@@ -0,0 +1,17 @@
+### Diccionarios ###
+
+my_dict = dict()
+
+print(type(my_dict))
+
+my_other_dict = {"Nombre":"Rafa", "Apellido":"Delgado", "Edad":34, 1:"Python"}
+
+my_dict = {
+    "Nombre":"Rafa",
+    "Apellido":"Delgado",
+    "Edad":34,
+    "Lenguajes": {"Python","Swift","JavaScript"}
+    }
+
+print(my_other_dict)
+print(my_dict)
