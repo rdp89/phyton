b56f7941ac6b2f508fe3ad09c50e1e4fc9232675
rdp89 <rafadelgado89@gmail.com>
2024-02-06 12:24:09 +0100

    Terminamos excepciones

--- 12_exceptions.py
+++ 12_exceptions.py
@@ -1,10 +1,50 @@
 ### Exceptions Handling ###
 
 numberOne = 10
+#numberTwo = 20
 numberTwo = "20"
 
+#try except
+try:
+    print(numberOne + numberTwo) #Intentamos sumar un int y un string
+    print("No se ha producido error")
+except:
+    print("Se ha producido un error, cambia el tipo de la variable a tipo int") #Aquí llega si se produce la excepción
+
+#try except else
+    
 try:
     print(numberOne + numberTwo) #Intentamos sumar un int y un string
     print("No se ha producido error")
 except:
     print("Se ha producido un error, cambia el tipo de la variable a tipo int")
+else: #Se ejecuta si no se produce una excepción
+    print("La ejecución continúa correctamente")
+
+#try except else finally
+try:
+    print(numberOne + numberTwo) #Intentamos sumar un int y un string
+    print("No se ha producido error")
+except:
+    print("Se ha producido un error, cambia el tipo de la variable a tipo int")
+else: #Se ejecuta si no se produce una excepción
+    print("La ejecución continúa correctamente")
+finally: #Se ejecuta siempre, pase lo que pase
+    print("La ejecución continúa") #Tanto el else como el finally son opcionales
+    
+#Captura de excepciones por tipo
+    
+try:
+    print(numberOne + numberTwo) #Intentamos sumar un int y un string
+    print("No se ha producido error")
+except ValueError:
+    print("No se ha producido error tipo ValueError")
+except TypeError:
+    print("Se ha producido un error tipo TypeError")
+
+#Captura de la información de la excepción
+try:
+    print(numberOne + numberTwo) #Intentamos sumar un int y un string
+    print("No se ha producido error")
+except Exception as mierror: #Como variable (la podemos llamar como queramos sin usar palabras reservadas) podemos mostrar con un print los detalles del error
+    print(f"Se ha producido un error tipo TypeError, los detalles del error son los siguientes: {mierror}")
